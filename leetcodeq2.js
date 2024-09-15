c9d663c1749768afa81c0515626e93122c7728ac
Jacob <jacob.dietz1@g.austincc.edu>
2024-09-14 19:50:50 -0500

    Added isomorphic string problem

--- leetcodeq2.js
+++ leetcodeq2.js
@@ -104,3 +104,30 @@ function sortScores(unsortedScores, highestPossibleScore) {
 
     return !count;
   }
+
+
+  function isIsomorphic(firstString, secondString) {
+
+    // Check if the same length. If not, they cannot be isomorphic
+    if (firstString.length !== secondString.length) return false
+
+    const letterMap = {};
+
+    for (let i = 0; i < firstString.length; i++) {
+      let letterA = firstString[i],
+          letterB = secondString[i];
+
+      // If the letter does not exist, create a map and map it to the value
+      // of the second letter
+      if (letterMap[letterA] === undefined) {
+        letterMap[letterA] = letterB;
+      } else if (letterMap[letterA] !== letterB) {
+        // If letterA already exists in the map, but it does not map to
+        // letterB, that means that A is mapping to more than one letter.
+        return false;
+      }
+    }
+    // If after iterating through and conditions are satisfied, return true.
+    // They are isomorphic
+    return true;
+  }
