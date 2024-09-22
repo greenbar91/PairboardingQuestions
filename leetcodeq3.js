30a74b07a4d6d78fa9591a9d76154ec6b31671ac
Jacob <jacob.dietz1@g.austincc.edu>
2024-09-21 21:11:59 -0500

    Solved valid palidrome

--- leetcodeq3.js
+++ leetcodeq3.js
@@ -97,3 +97,38 @@ minStack.getMin(); // return -2
 // -231 <= val <= 231 - 1
 // Methods pop, top and getMin operations will always be called on non-empty stacks.
 // At most 3 * 104 calls will be made to push, pop, top, and getMin.
+
+// A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.
+
+// Given a string s, return true if it is a palindrome, or false otherwise.
+
+var isPalindrome = function(s) {
+    return filterAlphaNumeric(s) == filterAlphaNumeric(s).split('').reverse().join('')
+};
+
+const filterAlphaNumeric = (s, nonAlphaNumeric = new RegExp('[^a-z0-9]','gi')) => s
+    .toLowerCase()
+    .replace(nonAlphaNumeric, '')
+
+// Example 1:
+
+// Input: s = "A man, a plan, a canal: Panama"
+// Output: true
+// Explanation: "amanaplanacanalpanama" is a palindrome.
+// Example 2:
+
+// Input: s = "race a car"
+// Output: false
+// Explanation: "raceacar" is not a palindrome.
+// Example 3:
+
+// Input: s = " "
+// Output: true
+// Explanation: s is an empty string "" after removing non-alphanumeric characters.
+// Since an empty string reads the same forward and backward, it is a palindrome.
+
+
+// Constraints:
+
+// 1 <= s.length <= 2 * 105
+// s consists only of printable ASCII characters.
