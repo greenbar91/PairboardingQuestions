79d6a6266fdde4fe8a7258afcdbbd7d1ca188b42
Jacob <jacob.dietz1@g.austincc.edu>
2024-09-21 21:10:18 -0500

    Added maxProfit

---
+++ leetcodeq3.js
@@ -0,0 +1,99 @@
+// You are given an array prices where prices[i] is the price of a given stock on the ith day.
+
+// You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+
+// Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+
+const maxProfit = function(arr){
+
+    if(arr.length === 1){
+        return 0
+    }
+
+    let min = arr[0]
+    let max = 0
+
+    for(let i =0; i < arr.length; i++){
+        if(arr[i] < min){
+            min = arr[i]
+        }
+        else if ((arr[i] - min) > max){
+            max = arr[i] - min
+        }
+    }
+
+    return max
+}
+
+
+
+// Example 1:
+
+// prices = [7,1,5,3,6,4]
+// Output: 5
+// Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+// Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
+// Example 2:
+
+// prices = [7,6,4,3,1]
+// console.log(maxProfit(prices))
+// Output: 0
+// Explanation: In this case, no transactions are done and the max profit = 0.
+
+
+// Constraints:
+
+// 1 <= prices.length <= 105
+// 0 <= prices[i] <= 104
+
+
+// Code
+// Testcase
+// Test Result
+// Test Result
+// 155. Min Stack
+// Medium
+// Topics
+// Companies
+// Hint
+// Design a stack that supports push, pop, top, and retrieving the minimum element in constant time.
+
+// Implement the MinStack class:
+
+// MinStack() initializes the stack object.
+// void push(int val) pushes the element val onto the stack.
+// void pop() removes the element on the top of the stack.
+// int top() gets the top element of the stack.
+// int getMin() retrieves the minimum element in the stack.
+// You must implement a solution with O(1) time complexity for each function.
+
+const MinStack = function(){
+
+}
+
+
+// Example 1:
+
+// Input
+// ["MinStack","push","push","push","getMin","pop","top","getMin"]
+// [[],[-2],[0],[-3],[],[],[],[]]
+
+// Output
+// [null,null,null,null,-3,null,0,-2]
+
+// Explanation
+// MinStack minStack = new MinStack();
+// minStack.push(-2);
+// minStack.push(0);
+// minStack.push(-3);
+// minStack.getMin(); // return -3
+// minStack.pop();
+// minStack.top();    // return 0
+// minStack.getMin(); // return -2
+
+
+// Constraints:
+
+// -231 <= val <= 231 - 1
+// Methods pop, top and getMin operations will always be called on non-empty stacks.
+// At most 3 * 104 calls will be made to push, pop, top, and getMin.
