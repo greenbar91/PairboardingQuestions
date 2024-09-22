2fb04ba4ed597109b9389673cb131e6dd912b106
Jacob <jacob.dietz1@g.austincc.edu>
2024-09-21 21:10:59 -0500

    Figuring out stack problem

--- leetcodeq3.js
+++ leetcodeq3.js
@@ -82,14 +82,14 @@ const MinStack = function(){
 // [null,null,null,null,-3,null,0,-2]
 
 // Explanation
-// MinStack minStack = new MinStack();
-// minStack.push(-2);
-// minStack.push(0);
-// minStack.push(-3);
-// minStack.getMin(); // return -3
-// minStack.pop();
-// minStack.top();    // return 0
-// minStack.getMin(); // return -2
+MinStack minStack = new MinStack();
+minStack.push(-2);
+minStack.push(0);
+minStack.push(-3);
+minStack.getMin(); // return -3
+minStack.pop();
+minStack.top();    // return 0
+minStack.getMin(); // return -2
 
 
 // Constraints:
