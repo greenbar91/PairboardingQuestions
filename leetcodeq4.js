28b53fbcff4f3f3a5caf280aeea5b44c48347526
Jacob <jacob.dietz1@g.austincc.edu>
2024-09-28 21:41:37 -0500

    added reverse ll

--- leetcodeq4.js
+++ leetcodeq4.js
@@ -40,3 +40,26 @@ var middleNode = function(head) {
 
     return slowPointer;
 };
+
+
+class Solution {
+    reverseList(head) {
+        // Initialize pointers
+        let prev = null; // Previous node starts as null
+        let curr = head; // Current node starts at the head
+
+        // Traverse the list
+        while (curr !== null) {
+            let next = curr.next; // Save the next node
+
+            curr.next = prev; // Reverse the link
+
+            // Move pointers forward
+            prev = curr; // Move prev to the current node
+            curr = next; // Move curr to the next node
+        }
+
+        // prev is now the new head of the reversed list
+        return prev;
+    }
+}
