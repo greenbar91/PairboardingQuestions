6ec24eeacfba23f315e884a5e61a728c3c026b5c
Jacob <jacob.dietz1@g.austincc.edu>
2024-09-28 21:40:57 -0500

    added middle node

--- leetcodeq4.js
+++ leetcodeq4.js
@@ -28,3 +28,15 @@ class Solution {
         }
     }
 }
+
+var middleNode = function(head) {
+    let slowPointer = head;
+    let fastPointer = head;
+
+    while (fastPointer !== null && fastPointer.next !== null) {
+        slowPointer = slowPointer.next;
+        fastPointer = fastPointer.next.next;
+    }
+
+    return slowPointer;
+};
