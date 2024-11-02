3e6aff03de027a206be2df9ba2cbacc8b1398709
Jacob <jacob.dietz1@g.austincc.edu>
2024-11-02 18:39:07 -0500

    added search2dmatrix

--- leetcode7.js
+++ leetcode7.js
@@ -7,14 +7,33 @@ class Solution {
     binary_search(l, r, nums, target) {
         if (l > r) return -1;
         let m = l + Math.floor((r - l) / 2);
-        
+
         if (nums[m] === target) return m;
-        return (nums[m] < target) ? 
-            this.binary_search(m + 1, r, nums, target) : 
+        return (nums[m] < target) ?
+            this.binary_search(m + 1, r, nums, target) :
             this.binary_search(l, m - 1, nums, target);
     }
 
     search(nums, target) {
         return this.binary_search(0, nums.length - 1, nums, target);
+    }
+}
+
+
+class Solution {
+    /**
+     * @param {number[][]} matrix
+     * @param {number} target
+     * @return {boolean}
+     */
+    searchMatrix(matrix, target) {
+        for (let r = 0; r < matrix.length; r++) {
+            for (let c = 0; c < matrix[r].length; c++) {
+                if (matrix[r][c] == target) {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 }
