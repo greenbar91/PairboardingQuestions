ce09d66c9b11754b57224b7320ec681de5f6ea83
Jacob <jacob.dietz1@g.austincc.edu>
2024-12-14 20:55:55 -0600

    anagram

--- leetcode11.js
+++ leetcode11.js
@@ -216,3 +216,20 @@ class Solution {
         return false;
     }
 }
+
+class Solution {
+    /**
+     * @param {string} s
+     * @param {string} t
+     * @return {boolean}
+     */
+    isAnagram(s, t) {
+        if (s.length !== t.length) {
+            return false;
+        }
+
+        let sSort = s.split("").sort().join();
+        let tSort = t.split("").sort().join();
+        return sSort == tSort
+    }
+}
