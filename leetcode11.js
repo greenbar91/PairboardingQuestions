3459de7794a4a658f7f9331939edb54e985115f3
Jacob <jacob.dietz1@g.austincc.edu>
2024-12-14 20:55:35 -0600

    dupe

--- leetcode11.js
+++ leetcode11.js
@@ -199,3 +199,20 @@ class Solution {
         return fresh === 0 ? time : -1;
     }
 }
+
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {boolean}
+     */
+    hasDuplicate(nums) {
+        for (let i = 0; i < nums.length; i++) {
+            for (let j = i + 1; j < nums.length; j++) {
+                if (nums[i] === nums[j]) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
