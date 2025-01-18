73ab470d0c2f089552c043fa29ffc18f2f38f38f
Jacob <jacob.dietz1@g.austincc.edu>
2025-01-18 17:10:40 -0600

    eval LPRN

--- leetcod14.js
+++ leetcod14.js
@@ -72,3 +72,31 @@ class MinStack {
         return this.min;
     }
 }
+
+class Solution {
+    /**
+     * @param {string[]} tokens
+     * @return {number}
+     */
+    evalRPN(tokens) {
+        const stack = [];
+        for (const c of tokens) {
+            if (c === '+') {
+                stack.push(stack.pop() + stack.pop());
+            } else if (c === '-') {
+                const a = stack.pop();
+                const b = stack.pop();
+                stack.push(b - a);
+            } else if (c === '*') {
+                stack.push(stack.pop() * stack.pop());
+            } else if (c === '/') {
+                const a = stack.pop();
+                const b = stack.pop();
+                stack.push(Math.trunc(b / a));
+            } else {
+                stack.push(parseInt(c));
+            }
+        }
+        return stack.pop();
+    }
+}
