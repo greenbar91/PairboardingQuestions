820611255fe1e23b529b107fa8cb8011425f03c0
Jacob <jacob.dietz1@g.austincc.edu>
2025-02-01 20:57:15 -0600

    canjump

--- leetcode16.js
+++ leetcode16.js
@@ -13,3 +13,26 @@ class Solution {
         return dfs(0, false);
     }
 }
+
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {boolean}
+     */
+    canJump(nums) {
+        const dfs = (i) => {
+            if (i === nums.length - 1) {
+                return true;
+            }
+            const end = Math.min(nums.length - 1, i + nums[i]);
+            for (let j = i + 1; j <= end; j++) {
+                if (dfs(j)) {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        return dfs(0);
+    }
+}
