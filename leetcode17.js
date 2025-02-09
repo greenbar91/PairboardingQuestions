bfb97250b1698b90456abc08964479594a083df2
Jacob <jacob.dietz1@g.austincc.edu>
2025-02-08 20:03:16 -0600

    min eating speed

--- leetcode17.js
+++ leetcode17.js
@@ -36,3 +36,25 @@ class Solution {
         return false;
     }
 }
+
+class Solution {
+    /**
+     * @param {number[]} piles
+     * @param {number} h
+     * @return {number}
+     */
+    minEatingSpeed(piles, h) {
+        let speed = 1;
+        while (true) {
+            let totalTime = 0;
+            for (let pile of piles) {
+                totalTime += Math.ceil(pile / speed);
+            }
+
+            if (totalTime <= h) {
+                return speed;
+            }
+            speed++;
+        }
+    }
+}
