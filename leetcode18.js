e7cddff21797446cdefd6eb5bbfa31cc3b958b3f
Jacob <jacob.dietz1@g.austincc.edu>
2025-02-15 19:44:46 -0600

    levelOrder

--- leetcode18.js
+++ leetcode18.js
@@ -105,3 +105,32 @@ class Solution {
         return null;
     }
 }
+
+class Solution {
+    /**
+     * @param {TreeNode} root
+     * @return {number[][]}
+     */
+    levelOrder(root) {
+        let res = [];
+
+        /**
+         * @param {TreeNode} node
+         * @param {number} depth
+         */
+        function dfs(node, depth) {
+            if (!node) return;
+
+            if (res.length === depth) {
+                res.push([]);
+            }
+
+            res[depth].push(node.val);
+            dfs(node.left, depth + 1);
+            dfs(node.right, depth + 1);
+        }
+
+        dfs(root, 0);
+        return res;
+    }
+}
