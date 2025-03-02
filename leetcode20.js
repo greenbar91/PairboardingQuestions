a700d43083ba9c65d933788dc56f05db52c6b132
Jacob <jacob.dietz1@g.austincc.edu>
2025-03-02 00:00:40 -0600

    jupm

--- leetcode20.js
+++ leetcode20.js
@@ -36,3 +36,26 @@ class Solution {
         return dfs(0);
     }
 }
+
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number}
+     */
+    jump(nums) {
+        const dfs = (i) => {
+            if (i === nums.length - 1) {
+                return 0;
+            }
+            if (nums[i] === 0) return 1000000;
+            let res = 1000000;
+            const end = Math.min(nums.length - 1, i + nums[i]);
+            for (let j = i + 1; j <= end; j++) {
+                res = Math.min(res, 1 + dfs(j));
+            }
+            return res;
+        }
+
+        return dfs(0);
+    }
+}
