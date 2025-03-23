d494af0ba6d710ea1803fc7122a7812b259ed472
Jacob <jacob.dietz1@g.austincc.edu>
2025-03-22 23:15:57 -0500

    cloneGraph

--- leetcode23.js
+++ leetcode23.js
@@ -64,3 +64,38 @@ class Solution {
         return area;
     }
 }
+
+class Solution {
+    /**
+     * @param {Node} node
+     * @return {Node}
+     */
+    cloneGraph(node) {
+        const oldToNew = new Map();
+        return this.dfs(node, oldToNew);
+    }
+
+    /**
+     * @param {Node} node
+     * @param {Map} oldToNew
+     * @return {Node}
+     */
+    dfs(node, oldToNew) {
+        if (node === null) {
+            return null;
+        }
+
+        if (oldToNew.has(node)) {
+            return oldToNew.get(node);
+        }
+
+        const copy = new Node(node.val);
+        oldToNew.set(node, copy);
+
+        for (const nei of node.neighbors) {
+            copy.neighbors.push(this.dfs(nei, oldToNew));
+        }
+
+        return copy;
+    }
+}
